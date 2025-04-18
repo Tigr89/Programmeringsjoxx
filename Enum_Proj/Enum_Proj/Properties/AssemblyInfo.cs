d99e56603b117e5076afa1a1b7be1ff28aa0ed2d
Tigr89 <123071532+Tigr89@users.noreply.github.com>
2025-04-18 12:43:27 +0200

    Restarted on new branch

--- Enum_Proj/Enum_Proj/Properties/AssemblyInfo.cs
+++ Enum_Proj/Enum_Proj/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FileManagement")]
+[assembly: AssemblyTitle("Enum_Proj")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FileManagement")]
+[assembly: AssemblyProduct("Enum_Proj")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e30b9543-5581-493d-ba94-33764906599d")]
+[assembly: Guid("3cbf603e-4950-4b08-ab50-7a5b6e271982")]
 
 // Version information for an assembly consists of the following four values:
 //
